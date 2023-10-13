0e6a5b0f16081d2497c78896656c3340e745e9e5
Ivan Saavedra <78055107+ivanrsv85@users.noreply.github.com>
2023-10-13 02:03:48 +0000

    asdf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def main():
     st.title("Mi Web App: Grupo 6")
     
     st.markdown("""
-        <iframe width="1200" height="900" src="https://lookerstudio.google.com/embed/reporting/4d121c56-cc84-4ced-aa70-b4f09dc0709d/page/u91eD" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe width="1200" height="1000" src="https://lookerstudio.google.com/embed/reporting/4d121c56-cc84-4ced-aa70-b4f09dc0709d/page/u91eD" frameborder="0" style="border:0" allowfullscreen></iframe>
         """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
